55a22dd87df94db8b7d6f0680273531aebedf7ff
Sean F Chan <seanfchan@gmail.com>
2011-10-11 22:27:55 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vent::Application.config.secret_token = '7bd0f7563e8b61b11ed672cec7c2e9a50e800819d41830cdbf11e254b8e70b796c0998ef48c12150f9f7b10cb9bd31a32d53d724f7bc4933628970323dd0bc87'
