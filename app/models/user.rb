b3fddc2b0ca31c1bf8a0b9e7a7e2be1f1e719b6b
Sean F Chan <seanfchan@gmail.com>
2011-10-14 15:41:49 -0700

    Created a User model

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  # Define which attributes are accessible to the user
+  attr_accessible :name, :email
+end
