42aa8fe1a7a1ba2e302cf4a09d9ca4e05981416c
Sean F Chan <seanfchan@gmail.com>
2011-10-16 00:44:05 -0700

    Working user sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
       @title = 'Sign in'
       render 'new'
     else
-      # do soemthing else
+      sign_in user
+      redirect_to user_path(user)
     end
   end
 
