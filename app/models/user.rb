453b94023ffa6d92ec7f44531aad9b3405050dc6
Sean F Chan <seanfchan@gmail.com>
2011-11-03 01:05:34 -0700

    Fixed encrypted password changing when toggleing user :admin => true

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,8 +80,10 @@ class User < ActiveRecord::Base
   private
 
     def encrypt_password
-      self.salt = make_salt if new_record?
-      self.encrypted_password = encrypt(password)
+      if password.present?
+        self.salt = make_salt if new_record?
+        self.encrypted_password = encrypt(password)
+      end
     end
 
     def make_salt
