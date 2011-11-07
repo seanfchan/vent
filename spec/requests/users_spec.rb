844e62e11b677326466ebb4a799b2a491b843b13
Sean F Chan <seanfchan@gmail.com>
2011-11-06 18:01:03 -0800

    Changed the signin label names

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,14 +4,26 @@ DatabaseCleaner.strategy = :truncation
 
 describe "Users" do
   describe 'sign up' do
+    it 'should have the correct labels' do
+      visit signup_path
+      response.should have_selector('div.field>label',
+                                    :content => 'Screen Name')
+      response.should have_selector('div.field>label',
+                                    :content => 'Email')
+      response.should have_selector('div.field>label',
+                                    :content => 'Password')
+      response.should have_selector('div.field>label',
+                                    :content => 'Retype Password')
+    end
+
     describe 'failure' do
       it 'should not make a new user' do
         lambda do
           visit signup_path
-          fill_in 'Name',         :with => ''
-          fill_in 'Email',        :with => ''
-          fill_in 'Password',     :with => ''
-          fill_in 'Confirmation', :with => ''
+          fill_in 'Screen Name',      :with => ''
+          fill_in 'Email',            :with => ''
+          fill_in 'Password',         :with => ''
+          fill_in 'Retype Password',  :with => ''
           click_button
           response.should render_template('users/new')
           response.should have_selector('div#error_explanation')
@@ -23,10 +35,10 @@ describe "Users" do
       it 'should make a new user' do
         lambda do
           visit signup_path
-          fill_in 'Name',         :with => "Sean"
-          fill_in 'Email',        :with => "user@example.com"
-          fill_in 'Password',     :with => "foobar"
-          fill_in 'Confirmation', :with => "foobar"
+          fill_in 'Screen Name',     :with => "Sean"
+          fill_in 'Email',           :with => "user@example.com"
+          fill_in 'Password',        :with => "foobar"
+          fill_in 'Retype Password', :with => "foobar"
           click_button
           response.should have_selector('div.flash.success', 
                                         :content => 'Welcome')
